74eee45fbc5ed36280dadc2f1c0de41c323f1596
Sarah Oliveira <soliveirarm@outlook.com>
2024-05-22 15:53:36 -0300

    feat: change button color when phase changes

--- src/components/ToggleTimerBtn.jsx
+++ src/components/ToggleTimerBtn.jsx
@@ -1,13 +1,10 @@
 /* eslint-disable react/prop-types */
 import { FaPlay, FaPause } from "react-icons/fa"
 
-export default function ToggleTimerBtn({ onClick, isActive }) {
+export default function ToggleTimerBtn({ onClick, started, color }) {
   return (
-    <button
-      className="flex justify-center items-center text-3xl  w-40 h-16 rounded-lg  transition-all text-white bg-transparent border-2 border-white hover:scale-110"
-      onClick={onClick}
-    >
-      {isActive ? <FaPause /> : <FaPlay />}
+    <button className={`btn ${started ? "-started" : color}`} onClick={onClick}>
+      {started ? <FaPause /> : <FaPlay />}
     </button>
   )
 }
